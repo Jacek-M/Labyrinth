f74431fea5f71c16ddf02c744e87638295777bfe
Karol Miksztal <fecx888@gmail.com>
2016-03-13 12:27:13 +0100

    Test
    testowy up

--- Labyrinth/src/labyrinth/GUI.java
+++ Labyrinth/src/labyrinth/GUI.java
@@ -4,4 +4,14 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame {
     
+    public static final String TITLE = "Labyrinth";
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 500;
+    
+    
+    public GUI() {
+        this.setTitle(TITLE);
+        this.setSize(WIDTH, HEIGHT);
+        this.setResizable(false);
+    }
 }
--- Labyrinth/src/labyrinth/Runner.java
+++ Labyrinth/src/labyrinth/Runner.java
@@ -1,16 +1,9 @@
 package labyrinth;
 
 public class Runner {
-
-    public static final String TITLE = "Labyrinth";
-    public static final int WIDTH = 300;
-    public static final int HEIGHT = 500;
-
+    
     public static void main(String[] args) {
         GUI gui = new GUI();
-        gui.setTitle(TITLE);
-        gui.setSize(WIDTH, HEIGHT);
-        gui.setResizable(false);
         gui.setVisible(true);
     }
 }
